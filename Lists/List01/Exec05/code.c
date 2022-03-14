a6c9bd6deeddd34ee1dbbbb60a0f5dbeea380f15
RenanGAS <renansakaoki@gmail.com>
2022-03-14 13:41:31 -0300

    feat: do the Exec05, the 'Flood fill Algorithm'

--- Lists/List01/Exec05/code.c
+++ Lists/List01/Exec05/code.c
@@ -2,8 +2,50 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-bool Explorar(A, i, j)
+bool Explorar(int A[10][10],int i,int j)
 {
+    if (i > 9 || i < 0 || j > 9 || j < 0)
+    {
+        return false;
+    }
+
+    if (A[i][j] == 1 || A[i][j] == 2)
+    {
+        return false;
+    }
+
+    A[i][j] = 2;
+
+    if (A[i][j] == A[9][9])
+    {
+        return true;
+    }
+
+    if (Explorar(A, i - 1, j))
+    {
+        printf(" (%d, %d) ", i-1, j);
+        return true;
+    }
+
+    if (Explorar(A, i + 1, j))
+    {
+        printf(" (%d, %d) ", i+1, j);
+        return true;
+    }
+
+    if (Explorar(A, i, j - 1))
+    {
+        printf(" (%d, %d) ", i, j - 1);
+        return true;
+    }
+
+    if (Explorar(A, i, j + 1))
+    {
+        printf(" (%d, %d) ", i, j + 1);
+        return true;
+    }
+    
+    return false;
 }
 
 int main()
@@ -20,13 +62,19 @@ int main()
         {1, 0, 0, 0, 0, 0, 0, 1, 0, 1},
         {1, 1, 1, 1, 1, 1, 1, 1, 0, 0},
     };
-
+    
+    printf("\n");
+    
     bool res = Explorar(A, 0, 0);
 
     if (!res)
     {
         printf("\nNão existe um caminho para a saída!\n\n");
     }
-
+    else
+    {
+        printf(" (0, 0)\n\n");
+    }
+    
     return EXIT_SUCCESS;
 }
