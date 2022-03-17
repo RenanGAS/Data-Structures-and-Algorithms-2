34add83a6f23f6aa26543511d51af37869254591
RenanGAS <renansakaoki@gmail.com>
2022-03-17 08:37:12 -0300

    feat: do the Exec6-1, the exponential function

---
+++ Lists/List01/Exec6-1/code.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float expo(float x, unsigned int n)
+{
+    if (n == 1)
+    {
+        return x;
+    }
+
+    return x * expo(x, n - 1);
+}
+
+int main()
+{
+    printf("\nCálculo do exponencial natural de um número real através de uma Função Recursiva\n");
+    
+    float x = 0;
+    printf("\nDigite um número: ");
+    scanf("%f", &x);
+
+    unsigned int n = 0;
+    printf("\nDigite seu expoente: ");
+    scanf("%u", &n);
+
+    float res = expo(x, n);
+
+    printf("\n%.2f^%u = %.2f\n\n", x, n, res);
+
+    return EXIT_SUCCESS;
+}
