d22eb84421727a42aa22d1d113d4d3c5ae1757c7
RENAN GUENSUKE AOKI SAKASHITA <a1903934@debian.dacom>
2022-03-24 19:18:33 -0300

    feat: continue the Merge Sort's Algorithm

--- Activities/Sorting Algorithms/merge_sort.c
+++ Activities/Sorting Algorithms/merge_sort.c
@@ -1,17 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
+
+void copy_vector(int *v_x, int *v, int i, int f)
+{
+    for (i = i; i < f; i++)
+    {
+        if (i == f - 1)
+        {
+            v_x[i] = INT_MAX;
+        }
+        else
+        {
+            v_x[i] = v[i];
+        }
+    }
+}
 
 void merge_vectors(int *v, int p, int q, int r)
 {
+    int *v_e = calloc(q + 2, sizeof(int));
+    int *v_d = calloc((r - q) + 1, sizeof(int));
+
+    copy_vector(v_e, v, p, q + 1);
+    copy_vector(v_d, v, q, r + 1);
+
+    int i = 0;
+    int j = 0;
+
+    for (int k = 0; k <= r; k++)
+    {
+        if (v_e[i] < v_d[j])
+        {
+            v[k] = v_e[i];
+            i++;
+        }
+        else
+        {
+            v[k] = v_d[j];
+            j++;
+        }
+    }
+
+    free(v_e);
+    free(v_d);
 }
 
 void merge_sort(int *vector, int e, int d)
 {
-    if(e < d){
+    if (e < d)
+    {
         int m = (e + d) / 2;
-        merge_sort(v, e, m);
-        merge_sort(v, m + 1, d);
-        merge_vectors(v, e, m, d);
+        merge_sort(vector, e, m);
+        merge_vectors(vector, e, m, d);
+        merge_sort(vector, m + 1, d);
+        merge_vectors(vector, e, m, d);
     }
 }
 
@@ -38,7 +81,9 @@ int main()
 
     printVector(v, 10);
 
-    printVector(merge_sort(v, 9), 10);
+    merge_sort(v, 0, 9);
+
+    printVector(v, 10);
 
     return EXIT_SUCCESS;
 }
