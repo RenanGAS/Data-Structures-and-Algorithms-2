bb85e8410ea60dbf967f093a87b750aef099a535
RENAN GUENSUKE AOKI SAKASHITA <a1903934@debian.dacom>
2022-03-28 19:15:16 -0300

    feat: do the Sort, NoRepeat and Delete functions

--- Activities/ShopList/main.c
+++ Activities/ShopList/main.c
@@ -5,9 +5,14 @@
 
 int main()
 {
-    char list[MAX_ITEMS] = "agua leite sucrilos suco";
+    char list[MAX_ITEMS] = "agua leite sucrilos suco carne carne sucrilos laranja uva melancia laranja";
     List *new_list = Create(list);
     Print(new_list);
+    Sort(new_list);
+    Print(new_list);
+    char *new_list_ = NoRepeat(new_list);
+    printf("\nLista de Compras: %s\n", new_list_);
+    Delete(new_list);
 
     return EXIT_SUCCESS;
 }
--- Activities/ShopList/shoplist.c
+++ Activities/ShopList/shoplist.c
@@ -28,12 +28,88 @@ List *Create(char *list)
 
     new_list->n = i;
 
+    printf("\nLista criada.\n");
+
     return new_list;
 }
 
-void Sort(List *l);
+static void merge(char **v, int p, int q, int r)
+{
+    char **e, **d;
+    int i, j, k;
+    int n1 = q - p + 1;
+    int n2 = r - q;
+    char sentinela[] = "zzzzzz";
+    e = malloc(sizeof(char *) * (n1 + 1));
+    d = malloc(sizeof(char *) * (n2 + 1));
+    for (i = 0; i < n1; i++)
+        e[i] = v[p + i];
+    for (j = 0; j < n2; j++)
+        d[j] = v[q + 1 + j];
+    e[n1] = sentinela;
+    d[n2] = sentinela;
+    i = 0;
+    j = 0;
+    for (k = p; k <= r; k++)
+    {
+        if (strcmp(e[i], d[j]) <= 0)
+        {
+            v[k] = e[i];
+            i++;
+        }
+        else
+        {
+            v[k] = d[j];
+            j++;
+        }
+    }
+    free(e);
+    free(d);
+}
 
-char *NoRepeat(List *l);
+static void MergeSort(char **v, int e, int d)
+{
+    if (e < d)
+    {
+        int m = (e + d) / 2;
+        MergeSort(v, e, m);
+        MergeSort(v, m + 1, d);
+        merge(v, e, m, d);
+    }
+}
+
+void Sort(List *l)
+{
+    MergeSort(l->items, 0, l->n - 1);
+
+    printf("\nLista ordenada.\n");
+}
+
+char *NoRepeat(List *l)
+{
+    Sort(l);
+    char *max_list = malloc(BUF_LEN * sizeof(char));
+    max_list[0] = '\0';
+    strcat(max_list, l->items[0]);
+    strcat(max_list, " ");
+
+    char ref[21] = "";
+    strcpy(ref, l->items[0]);
+
+    for (int i = 1; i < l->n; i++)
+    {
+        if (strcmp(l->items[i], ref) != 0)
+        {
+            strcat(max_list, l->items[i]);
+            strcat(max_list, " ");
+            strcpy(ref, l->items[i]);
+        }
+    }
+
+    printf("\nElementos repetidos deletados.\n");
+
+    return max_list;
+}
 
 void Print(List *l)
 {
@@ -52,4 +128,15 @@ void Print(List *l)
     }
 }
 
-void Delete(List *l);
+void Delete(List *l)
+{
+    for (int i = 0; i < l->n; i++)
+    {
+        free(l->items[i]);
+    }
+
+    free(l->items);
+    free(l);
+
+    printf("\nLista deletada.\n\n");
+}
