29766bcc74394f8505c16c5f51903601e8eaa5ca
RenanGAS <renansakaoki@gmail.com>
2022-03-28 15:59:05 -0300

    feat: do the Create and Print functions and test them

--- Activities/ShopList/main.c
+++ Activities/ShopList/main.c
@@ -1,5 +1,13 @@
 #include "shoplist.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+
 int main()
 {
+    char list[MAX_ITEMS] = "agua leite sucrilos suco";
+    List *new_list = Create(list);
+    Print(new_list);
+
+    return EXIT_SUCCESS;
 }
--- Activities/ShopList/shoplist.c
+++ Activities/ShopList/shoplist.c
@@ -1,4 +1,7 @@
 #include "shoplist.h"
+
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 struct list
@@ -7,17 +10,46 @@ struct list
     int n;
 };
 
-List *Create(char *items)
+List *Create(char *list)
 {
-    List *new_list = calloc(sizeof(List));
-    new_list->items = calloc(MAX_ITEMS, sizeof(char));
-    char *item = strtok(new_list->items, " ");
+    List *new_list = malloc(sizeof(List));
+    new_list->items = malloc(MAX_ITEMS * sizeof(char));
+    char *item = strtok(list, " ");
+
+    int i = 0;
+
+    while (item != NULL)
+    {
+        new_list->items[i] = malloc(MAX_TAM * sizeof(char));
+        strcpy(new_list->items[i], item);
+        i++;
+        item = strtok(NULL, " ");
+    }
+
+    new_list->n = i;
+
+    return new_list;
 }
 
 void Sort(List *l);
 
 char *NoRepeat(List *l);
 
-void Print(List *l);
+void Print(List *l)
+{
+    printf("\nLista de Compras:");
+
+    for (int i = 0; i < l->n; i++)
+    {
+        if (i != l->n - 1)
+        {
+            printf(" %s,", l->items[i]);
+        }
+        else
+        {
+            printf(" %s\n", l->items[i]);
+        }
+    }
+}
 
 void Delete(List *l);
