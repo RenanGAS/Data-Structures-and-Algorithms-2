3fd173d829ad66055acddd0f4566c086a3a590af
RENAN GUENSUKE AOKI SAKASHITA <a1903934@debian.dacom>
2022-04-01 20:33:03 -0300

    feat: add the Quick Sort's Algorithm

--- Activities/Sorting Algorithms/quick_sort.c
+++ Activities/Sorting Algorithms/quick_sort.c
@@ -18,17 +18,45 @@ void printVector(int *vector, int size)
     }
 }
 
+void changePos(int *v, int i, int j)
+{
+    int aux = v[j];
+    v[j] = v[i];
+    v[i] = aux;
+}
+
 int partition(int *v, int p, int r)
-{   
+{
+    int pv = v[r];
+    int i = p - 1;
+
+    for (int j = p; j < r; j++)
+    {
+        if (v[j] <= pv)
+        {
+            i++;
+            changePos(v, i, j);
+        }
+    }
+
+    changePos(v, i + 1, r);
+
+    return (i + 1);
 }
 
 void quickSort(int *v, int e, int d)
 {
+    if (e < d)
+    {
+        int r = partition(v, e, d);
+        quickSort(v, e, r - 1);
+        quickSort(v, r + 1, d);
+    }
 }
 
 int main()
 {
-    int v[15] = {9, 7, 5, 3, 0, 1, 2, 6, 12, 10, 11, 13, -1, 8, 20};
+    int v[15] = {9, 7, 5, 3, 0, 1, 2, 20, 12, 10, 11, 13, -1, 8, 6};
 
     printVector(v, 15);
 
