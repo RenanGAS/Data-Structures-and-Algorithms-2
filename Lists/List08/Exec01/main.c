cb6e59b0babd884ef32cad0c9889009bfce81e68
RenanGAS <renansakaoki@gmail.com>
2022-05-24 02:54:35 -0400

    to do: implement THEA_imprimir and test the functions

--- Lists/List08/Exec01/main.c
+++ Lists/List08/Exec01/main.c
@@ -2,36 +2,29 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#include "ilist.h"
 #include "hashtable_ea.h"
 
-int collisions = 0;
 
 int main(int argc, char** argv){
     
     int n, i, chave;    
-    enum probing_strategy ps;
 
     int seed = 0;
 
     n = argc > 1 ? atoi(argv[1]) : 10;
-    ps = argc > 2 ? atoi(argv[2]) : PS_LINEAR;
 
     srand(seed);
 
-    htea_ea* ht;
+    THEA* ht;
 
-    ht = HTEA_Criar(2, "primes1.dat", ps);
+    ht = THEA_Criar(20);
 
-    HTEA_Inserir(&ht, 10, 5);
-    HTEA_Inserir(&ht, 11, 7);
-    HTEA_Inserir(&ht, 3, 7);
+    THEA_Inserir(ht, 10, 5);
+    THEA_Inserir(ht, 11, 7);
+    THEA_Inserir(ht, 3, 7);
 
     for(i = 0; i < n; i++){
         chave = (rand() % n*10);
-        HTEA_Inserir(&ht, chave, chave + (rand() % 10));
+        THEA_Inserir(ht, chave, chave + (rand() % 10));
     }
-
-    printf("collisions = %d\n", collisions);
-
 }
