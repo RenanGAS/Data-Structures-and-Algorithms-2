4c05785aada225109563f536b09463bc59bcdfec
RenanGAS <renansakaoki@gmail.com>
2022-06-08 10:48:35 -0400

    feat: do the Exec01 a) and b), the create, insert, search and print functions of a Binary Search Tree

--- Lists/List10/ab.c
+++ Lists/List10/ab.c
@@ -1,9 +1,11 @@
 #include "ab.h"
 #include <stdlib.h>
+#include <stdio.h>
 
-AB AB_Criar(int dado, AB e, AB d){
+AB AB_Criar(int dado, AB e, AB d)
+{
     AB n;
-    
+
     n = malloc(sizeof(struct AB));
     n->dado = dado;
     n->esq = e;
@@ -12,10 +14,77 @@ AB AB_Criar(int dado, AB e, AB d){
     return n;
 }
 
-void AB_Destruir(AB A){
-    if(A == NULL)
+AB AB_Buscar(int dado, AB A)
+{
+    if (A == NULL)
+    {
+        return NULL;
+    }
+
+    if (A->dado == dado)
+    {
+        return A;
+    }
+
+    if (dado < A->dado)
+    {
+        return AB_Buscar(dado, A->esq);
+    }
+
+    return AB_Buscar(dado, A->dir);
+}
+
+void AB_Inserir(int dado, AB *A)
+{
+    if ((*A) == NULL)
+    {
+        *A = AB_Criar(dado, NULL, NULL);
         return;
-    AB_Destruir(A->esq);
-    AB_Destruir(A->dir);
-    free(A);
+    }
+    else
+    {
+        if (dado < (*A)->dado)
+        {
+            AB_Inserir(dado, &(*A)->esq);
+        }
+        else
+        {
+            AB_Inserir(dado, &(*A)->dir);
+        }
+    }
+}
+
+void AB_Imprimir(AB *A, int i, char p)
+{
+    if ((*A) == NULL)
+    {
+        return;
+    }
+
+    for (int j = 0; j < i; j++)
+    {
+        printf("\t");
+    }
+
+    printf("(%c) %d\n", p, (*A)->dado);
+
+    i++;
+    
+    AB_Imprimir(&(*A)->esq, i, 'e');
+
+    AB_Imprimir(&(*A)->dir, i, 'd');
+
+    i -= 2;
+}
+
+void AB_Destruir(AB *A)
+{
+    if (*A == NULL)
+    {
+        return;
+    }
+
+    AB_Destruir(&(*A)->esq);
+    AB_Destruir(&(*A)->dir);
+    free(*A);
 }
--- Lists/List10/ab.h
+++ Lists/List10/ab.h
@@ -1,13 +1,17 @@
 #ifndef __AB_H__
 #define __AB_H__
 
-typedef struct AB{
+typedef struct AB
+{
     struct AB *esq;
     struct AB *dir;
     int dado;
-} *AB;
+} * AB;
 
 AB AB_Criar(int dado, AB e, AB d);
-void AB_Destruir(AB A);
+AB AB_Buscar(int dado, AB A);
+void AB_Inserir(int dado, AB *A);
+void AB_Imprimir(AB *A, int i, char p);
+void AB_Destruir(AB *A);
 
 #endif
